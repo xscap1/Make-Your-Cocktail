56eae187b4b38ef9ac91055c391796afa7e2538c
theo-poujol <theo.poujol@etu.univ-amu.fr>
2019-03-11 00:09:14 +0100

    Login Completed

--- index.php
+++ index.php
@@ -9,7 +9,11 @@ start_page("Accueil");
 
 if(isset($_SESSION['user_id'])){
     echo ('<p> Bonjour ' . $_SESSION['user_id'] . ' bienvenue sur la page d\'accueil </p>');
+    echo("<button type='submit'> DECONNEXION </button>");
 }
 
-
+else {
+    echo('Veuillez vous connecter');
+    header('Location: login.php' );
+}
 end_page();
--- login.php
+++ login.php
@@ -1,12 +1,18 @@
 <?php
 
 session_start();
-
-
-
-$username  = $_POST['login'];
+$username = $_POST['username'];
 $password = $_POST['password'];
 
+
+
+
+
+/*if (empty($username) && empty($password)) {
+    ?> <script> alert('VIDE') </script>
+    <?php
+}
+*/
 
 // Valeur a retourner en cas de succès de connexion
 
@@ -32,61 +38,94 @@ $options = [
 
 
 
-try {
+    try {
 
-    // ESSAI CONNEXION BASE DE DONNÉES
-    $pdo = new PDO($dsn, $user, $pass, $options);
+        // ESSAI CONNEXION BASE DE DONNÉES
+        $pdo = new PDO($dsn, $user, $pass, $options);
 
-    $query = $pdo->prepare("SELECT * FROM USERS WHERE USERNAME=? AND PWD=?");
-    $query->execute(array($username,$password));
+        $sql = "SELECT ID FROM USERS WHERE USERNAME=? AND PWD=? ";
+        $query = $pdo->prepare($sql);
+        $query->execute(array($username,$password));
 
-    if($result = $query->fetch(PDO::FETCH_ASSOC)) {
-        $_SESSION['user_id'] = $result['user_id'];
-        $msgSuccess = "Bien connecté";
-        header('Location: index.php');
-        exit(0);
+        if($query->rowCount() >= 1) {
+            $_SESSION['user_id'] = $username;
+            $_SESSION['time_start_login'] = time();
+            header("Location: index.php");
+        } else {
+            $messeg = "Username/Password is wrong";
+            echo($messeg);
+        }
     }
 
-    else {
-        $msgSuccess = " Non connceté";
-        ?> <script> alert('L\'indentifiant ou le mot de passe est incorrect');
-                    window.location.href = 'index.php';
-           </script>
+            /*
+            ?> <script> alert('L\'indentifiant ou le mot de passe est incorrect');
+                window.location.href = 'index.php';
+            </script>
 
-        <?php
-    }
+            <?php
+        }
 
-    // AFFICHAGE D'UN UTILISATEUR POUR VÉRIFIER QUE TOUT FONCTIONNE
-   // $stmt = $pdo->query('SELECT * FROM USERS WHERE USERNAME` = ' . $username . ' AND PWD = ' .$password . ';');
+        // AFFICHAGE D'UN UTILISATEUR POUR VÉRIFIER QUE TOUT FONCTIONNE
+        // $stmt = $pdo->query('SELECT * FROM USERS WHERE USERNAME` = ' . $username . ' AND PWD = ' .$password . ';');
 
-    /*$stmt = $pdo->query("SELECT * FROM USERS WHERE USERNAME =" . $pdo->quote($username) . " AND PWD = " . $pdo->quote($password) . ";"  );
-    $row = $stmt->fetch();
-    if ($stmt->rowCount() > 0) {
-        $retour->sucess = true;
-        session_start();
-        $_SESSION['user_id'] = $row['USERNAME']; //$username;
-        header('Location: index.php');
+        /*$stmt = $pdo->query("SELECT * FROM USERS WHERE USERNAME =" . $pdo->quote($username) . " AND PWD = " . $pdo->quote($password) . ";"  );
+        $row = $stmt->fetch();
+        if ($stmt->rowCount() > 0) {
+            $retour->sucess = true;
+            session_start();
+            $_SESSION['user_id'] = $row['USERNAME']; //$username;
+            header('Location: index.php');
 
-    }
+        }
 
-    else {
+        else {
 
 
-<?php
+    <?php
+
+        }
+
+
 
     }*/
 
-
-
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+    catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
 
 /*header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
 
-echo json_encode(['message' => $msgSuccess]);*/
+echo json_encode(['message' => $msgSuccess]);
+
+*/
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Login </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="JS/JQuery/jquery-3.3.1.min.js"></script>
+    <script src="JS/main.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+
+</head>
+<body>
+
+<form method="post" action="login.php">
+    <input type="text" name="username" id="username" placeholder="username">
+    <input type="password" name="password" id="password" placeholder="password">
+    <button type="submit" name="buttonForm" id="buttonForm"> Se connecter </button>
+</form>
+
+
+</body>
+</html>
