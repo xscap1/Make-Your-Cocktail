0deabe92a1d0c6e8b05edda961d7f8d2c10ad17f
theo-poujol <theo.poujol@etu.univ-amu.fr>
2019-03-20 15:20:15 +0100

    Test body append into index.php from another js file

--- JS/ingredient.js
+++ JS/ingredient.js
@@ -9,6 +9,29 @@ class Ingredient {
 
 $(document).ready(() => {
 
+    $.ajax({
+        url : 'index.php'
+    }).done(() => {
 
+        $('body').append(
+            $('<button />').attr({
+                "name": "CoucouButton",
+                "id" : "CoucouButton",
+                "type": "submit"
+
+            }).css({
+                "background-color": "#0066ff",
+                "border": "none",
+                "color": "white",
+                "margin-top" : "30px",
+                "margin-left": "30px",
+                "padding": "10px 20px",
+                "text-align": "center",
+                "text-decoration": "none",
+                "display": "inline-block",
+                "font-size": "16px",
+            }).html('Coucou')
+        );
+    });
 
 });
