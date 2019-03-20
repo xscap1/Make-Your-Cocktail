5a33819f13fc2c0d8a8ccdad0c4f6214fc89a8d6
theo-poujol <theo.poujol@etu.univ-amu.fr>
2019-03-20 15:29:01 +0100

    Test n° 2

--- JS/ingredient.js
+++ JS/ingredient.js
@@ -8,30 +8,55 @@ class Ingredient {
 }
 
 $(document).ready(() => {
-
     $.ajax({
-        url : 'index.php'
-    }).done(() => {
+        url: 'is_connected.php',
 
-        $('body').append(
-            $('<button />').attr({
-                "name": "CoucouButton",
-                "id" : "CoucouButton",
-                "type": "submit"
+    }).done((data) =>{
 
-            }).css({
-                "background-color": "#0066ff",
-                "border": "none",
-                "color": "white",
-                "margin-top" : "30px",
-                "margin-left": "30px",
-                "padding": "10px 20px",
-                "text-align": "center",
-                "text-decoration": "none",
-                "display": "inline-block",
-                "font-size": "16px",
-            }).html('Coucou')
-        );
+        if(data.success===false) {
+            /* La personne n'est pas connectée */
+
+            // Gérer par l'index.
+
+
+        }
+
+        else {
+            /* La personne est connectée */
+
+            $.ajax({
+                url : 'index.php'
+            }).done(() => {
+                $('body').append(
+                    $('<button />').attr({
+                        "id" : "ingredientButton",
+                        "name": "ingredientButton",
+                        "type": "submit"
+
+                    }).css({
+                        "background-color": "#0066ff",
+                        "border": "none",
+                        "color": "white",
+                        "margin-left": "30px",
+                        "padding": "10px 20px",
+                        "text-align": "center",
+                        "text-decoration": "none",
+                        "display": "inline-block",
+                        "font-size": "16px",
+                    }).html('Créer ingrédient').click( () => {
+                        $.ajax({
+                            url : 'ingredient.php',
+
+                        })
+                    })
+
+                );
+
+            });
+        }
+
+
     });
+
 
 });
